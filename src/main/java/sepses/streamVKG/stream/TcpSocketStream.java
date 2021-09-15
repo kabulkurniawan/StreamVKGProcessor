668bc5a3dbd124a81f7e5b8198be3f8bc0fab824
Kabul Kurniawan <kabul.kurniawan@tuwien.ac.at>
2021-09-15 15:59:39 +0200

    modified query example

--- src/main/java/sepses/streamVKG/stream/TcpSocketStream.java
+++ src/main/java/sepses/streamVKG/stream/TcpSocketStream.java
@@ -41,11 +41,17 @@ public class TcpSocketStream extends DataStreamImpl implements Runnable  {
             System.out.println("New client connected");
            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String line;
+            ArrayList<String> trip = new ArrayList<>();
+
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
-                Model dataModel = ModelFactory.createDefaultModel();
-                dataModel.read(IOUtils.toInputStream(line,"UTF-8"), null, "N3");
-                this.s.put(dataModel.getGraph(), System.currentTimeMillis());
+                trip.add(line);
+                if(trip.size()>=3) {
+                    Model dataModel = ModelFactory.createDefaultModel();
+                    dataModel.read(IOUtils.toInputStream(trip.get(0)+" "+trip.get(1)+" "+trip.get(2), "UTF-8"), null, "N3");
+                    this.s.put(dataModel.getGraph(), System.currentTimeMillis());
+                    trip.clear();
+                }
             }
 
         } catch (Exception e) {
